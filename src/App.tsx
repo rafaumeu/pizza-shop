74bc329b247bfbe9bc43586376012606e220d0af
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-13 16:50:11 -0300

    feat(setup): configure react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import './global.css'
 
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1>Pizza Shop</h1>
-      <Button>Add to cart</Button>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,3 @@
+export function SignIn() {
+  return <h1>Signin</h1>
+}
