e3eaf6ae5ea2a9e33cdae9a59ee0a510a48e4666
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-13 23:50:22 -0300

    fix(setup): resolve typescript and import alias configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
-import { defineConfig } from 'vite'
+import path from "node:path";
 
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
