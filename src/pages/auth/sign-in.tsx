0266aa7a26306c8b88716072e34e2ca340ad9a1d
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-14 00:43:09 -0300

    feat(auth): add react-hook-form and zod dependencies, update sign-in form

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,11 +1,28 @@
 import { Label } from '@radix-ui/react-label'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Input } from '@/components/ui/input'
 
 import { Button } from '../../components/ui/button'
 
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+  async function handleSignIn(data: SignInForm) {
+    console.log(data)
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+  }
   return (
     <>
       <Helmet title="Login" />
@@ -19,12 +36,12 @@ export function SignIn() {
             Acompanhe suas vendas pelo painel do parceiro
           </p>
         </div>
-        <form action="" className="space-y-4">
+        <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
           <div className="space-y-2">
             <Label htmlFor="email">Seu e-mail</Label>
-            <Input id="email" type="email" />
+            <Input id="email" type="email" {...register('email')} />
           </div>
-          <Button className="w-full" type="submit">
+          <Button disabled={isSubmitting} className="w-full" type="submit">
             Acessar painel
           </Button>
         </form>
