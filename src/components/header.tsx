682aad13fc037285250637d1266f63e02fda3d7e
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-14 21:14:13 -0300

    refactor(app): update application structure and header components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,12 @@ import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
 import { NavLink } from '@/components/nav-links'
 
+import { AccountMenu } from './account-menu'
+import { ThemeToggle } from './theme/theme-toggle'
+
 export function Header() {
   return (
-    <header className="border-b">
+    <div className="border-b">
       <div className="flex h-16 items-center gap-6 px-6">
         <Pizza className="h-6 w-6" />
         <Separator orientation="vertical" className="h-6" />
@@ -19,7 +22,12 @@ export function Header() {
             Pedidos
           </NavLink>
         </nav>
+
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+          <AccountMenu />
+        </div>
       </div>
-    </header>
+    </div>
   )
 }
