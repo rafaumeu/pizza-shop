056e6996a4be4c3154bf8d001018348ba61065d5
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-14 23:46:33 -0300

    refactor(ui): enhance button component styling and variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,6 +22,7 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-9 px-4 py-2',
+        xm: 'h-8 px-2.5',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
         icon: 'h-9 w-9',
@@ -55,3 +56,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 Button.displayName = 'Button'
 
 export { Button, buttonVariants }
+
