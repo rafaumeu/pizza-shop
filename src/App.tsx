cb129512137edc597946a8c94f49271837fc9632
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-16 02:28:05 -0300

    refactor(app): prepare app structure for API integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
+
+import { queryClient } from '@/lib/react-query'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
@@ -13,7 +16,9 @@ export function App() {
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Toaster richColors />
         <Helmet titleTemplate="%s | Pizza Shop" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
