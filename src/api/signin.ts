d46bbf836b2e959017c7a9923d7532beee5359ac
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-18 23:20:12 -0300

    refactor(auth): update signin method implementation

--- src/api/signin.ts
+++ src/api/signin.ts
@@ -1,8 +1,8 @@
-import { api } from "@/lib/axios"
+import { api } from "@/lib/axios";
 
 export type SignInBody = {
-  email: string
-}
-export async function signin({email}: SignInBody) {
-  await api.post('/authenticate', {email})
+  email: string;
+};
+export async function signin({ email }: SignInBody) {
+  await api.post("/authenticate", { email });
 }
