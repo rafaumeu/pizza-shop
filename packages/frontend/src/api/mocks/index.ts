d746b8e375486217dcabedd375b3842c403f633b
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-24 11:03:16 -0300

    feat(frontend): add MSW mock for sign-in API

--- packages/frontend/src/api/mocks/index.ts
+++ packages/frontend/src/api/mocks/index.ts
@@ -1,7 +1,8 @@
 import { setupWorker } from 'msw/browser'
 
+import { signInMock } from '@/api/mocks/sign-in-mock'
 import { env } from '@/env'
-export const worker = setupWorker()
+export const worker = setupWorker(signInMock)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') return
