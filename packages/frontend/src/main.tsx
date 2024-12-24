557a0090f77a48d4d370b2ef8a754a631cb67934
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-24 08:38:52 -0300

    fix(main): fix imports and application initialization

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { enableMSW } from '@/api/mocks/index.ts'
+
 import { App } from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+enableMSW().then(() => {
+  const rootElement = document.getElementById('root')
+  if (!rootElement) throw new Error('Root element not found')
+  const root = createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
