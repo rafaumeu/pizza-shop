fc06dbc00808a53daeed67ce17ee4f92ff794f89
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-12-24 11:03:16 -0300

    refactor(frontend): remove unused sign-in API

--- packages/frontend/src/api/signin.ts
+++
@@ -1,8 +0,0 @@
-import { api } from '@/lib/axios'
-
-export type SignInBody = {
-  email: string
-}
-export async function signin({ email }: SignInBody) {
-  await api.post('/authenticate', { email })
-}
